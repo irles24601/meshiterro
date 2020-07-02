f0d60c7dfb89f69081bd2d4d060f761dfcc67c45
vagrant <vagrant@localhost.localdomain>
2020-07-02 07:05:01 +0000

    [Finish] 5-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_prameters, if: :devise_controller?
+  protected
+  def configure_permitted_prameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
